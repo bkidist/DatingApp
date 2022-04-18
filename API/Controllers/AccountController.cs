9e73d605c36b2cbf36d3eaf6a491510096e5f857
bkidist <kidist.belay5@gmail.com>
2022-04-18 08:26:48 +0300

    kidist first git

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        {
+            if(await UserExists(registerDto.Username)) return BadRequest("Username Already Taken");
+            using var hmac = new HMACSHA512();
+            var user = new AppUser
+            {
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+            _context.Users.Add(user);   
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        // Check if user name is Exist
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+    }
+}
---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    
+    public class UsersController : BaseApiController
+    {
+        // we have to declear first in order to use it in Constructor 
+        // in Dependency injection
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            // here we have access to the database
+            _context = context;
+        }
+        [HttpGet]
+        // IEnumerable and List method has the same uses
+        // We use Async to make our db query fast while other quering no need to wait 
+     
+        //to get users
+        public async  Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            
+            return await _context.Users.ToListAsync();
+        }
+        // api/users/3
+        //to get individual user by id  
+        [HttpGet("{id}")]
+        // IEnumerable and List method has the same uses
+        public async Task<ActionResult<AppUser>> GetUsers(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+
+
+}
